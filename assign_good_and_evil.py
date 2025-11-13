00ec2e8afce983cbe1e08ed5f3a85f3b6108327b
wanhan05 <wanhansun22@gmail.com>
2025-11-12 23:12:07 -0800

    Implement graph labeling for good and evil nodes

--- assign_good_and_evil.py
+++ assign_good_and_evil.py
@@ -1,2 +1,25 @@
+from collections import deque
 def assign_good_and_evil(graph):
+    labels = {}
+    nodes = graph.nodes()
+    for start_node in nodes:
+        if start_node in labels:
+            continue
+        
+        queue = deque([start_node])
+        labels[start_node] = 'good'
+        
+        while queue:
+            current = queue.popleft()
+            current_label = labels[current]
+            opposite_label = 'evil' if current_label == 'good' else 'good'
+            for neighbor in graph.neighbors(current):
+                if neighbor in labels:
+                    if labels[neighbor] != opposite_label:
+                        return None
+                else:
+                    labels[neighbor] = opposite_label
+                    queue.append(neighbor)
+    
+    return labels
     
